674a66d32f03d602fc1c8dcdd3ba4c8d5f95dac4
meettan <mondal.tanmoy@synergicsoftek.com>
2026-01-21 15:49:25 +0530

    chnage

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const {createToken,verifyToken,newfuncttion} = require("./middleware/authMiddlew
 
 app.use('/v1/master', require('./router/master/indexRouter'));
 app.use('/v1/group', require('./router/group/indexGroupRouter'));
+app.use('/v1/member', require('./router/member/indexMemberRouter'));
 app.use('/v1/trans', require('./router/transacation/indexTransRouter'));
 app.use('/v1/depsav', require('./router/deposit_saving/indexdepsavingRouter'));
  
