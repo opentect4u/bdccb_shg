ec641d5acbefa3e55acdb7d39a70ebb39aca2b5c
meettan <mondal.tanmoy@synergicsoftek.com>
2026-01-21 18:55:58 +0530

    chnage

--- router/deposit_saving/dep_saving_Router.js
+++ router/deposit_saving/dep_saving_Router.js
@@ -22,13 +22,13 @@ const sahayikaCode = async (tenant_id) => {
 depsavingRouter.get("/deposit_list", async (req, res) => {
     try{
         const tenant_id = parseInt(req.query.tenant_id || 0);
-        const dist_id = parseInt(req.query.dist_id || 0);
+        const shg_id = parseInt(req.query.shg_id || 0);
         const sahayika_id = parseInt(req.query.sahayika_id || 0);
             // DIST ID AND BLOCK ID AND GP ID IS MANDATORY
-                if (!branch_id || branch_id <= 0 || !tenant_id || tenant_id <= 0) {
+                if (!shg_id || shg_id <= 0 || !tenant_id || tenant_id <= 0) {
                     return res.send({
                         success: false,
-                        msg: "dist id and tenant id is required"
+                        msg: "shg id and tenant id is required"
                     });
                 }
 
@@ -65,26 +65,20 @@ depsavingRouter.get("/deposit_list", async (req, res) => {
 });
 
 // save / edit group
-depsavingRouter.post("/save_sahayika", async (req, res) => {
+depsavingRouter.post("/save_sbacc", async (req, res) => {
     try {
-      const { sahayika_id,tenant_id,branch_id,dist_id,sahayika_name,phone_no,address,created_by,ip_address } = req.body;
-      let sahayika_gen_id = 0;
+      const {sb_id, tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,created_ip } = req.body;
       let datetime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-      if(sahayika_id == 0){
-          sahayika_gen_id = await sahayikaCode(tenant_id);
-      }else{
-          sahayika_gen_id = sahayika_id;
-      }
-      const table = "bdccb.md_sahayika";
-      const columns = sahayika_id > 0 ? ["tenant_id","branch_id","dist_id","sahayika_name","phone_no","address","modified_by","modified_at","ip_address"] : ["sahayika_id","tenant_id","branch_id","dist_id","sahayika_name","phone_no","address","created_by","created_at","ip_address"];
-      const values = sahayika_id > 0 ? [tenant_id,branch_id,dist_id,sahayika_name,phone_no,address,created_by,datetime,ip_address] : [sahayika_gen_id,tenant_id,branch_id,dist_id,sahayika_name,phone_no,address,created_by,datetime,ip_address];
-      const whereColumns = sahayika_id > 0 ? ["sahayika_id"] : [];
-      const whereValues = sahayika_id > 0 ? [sahayika_id] : [];
-      const flag = sahayika_id > 0 ? 1 : 0;
+      const table = "bdccb.td_deposit";
+      const columns = sb_id > 0 ? ["tenant_id","shg_id","branch_id","acc_no","acc_opening_dt","balance","modified_by","modified_at","modified_ip"] : ["tenant_id","shg_id","branch_id","acc_no","acc_opening_dt","balance","created_by","created_at","created_ip"];
+      const values = sb_id > 0 ? [tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,datetime,created_ip] : [tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,datetime,created_ip];
+      const whereColumns = sb_id > 0 ? ["sb_id"] : [];
+      const whereValues = sb_id > 0 ? [sb_id] : [];
+      const flag = sb_id > 0 ? 1 : 0;
       const result = await saveRecord(table, columns, values,whereColumns,whereValues,flag);
       return res.send({
         success: true,
-        msg: sahayika_id > 0 ? "Record Updated Successfully" : "Record Inserted Successfully",
+        msg: sb_id > 0 ? "Record Updated Successfully" : "Record Inserted Successfully",
         data: result.lastId
       });
       } catch (error) {
