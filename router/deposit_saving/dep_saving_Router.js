dbd7d0946d71f2bc1d507ad346ee9fba3067efe5
meettan <mondal.tanmoy@synergicsoftek.com>
2026-02-06 10:40:30 +0530

    chnage

--- router/deposit_saving/dep_saving_Router.js
+++ router/deposit_saving/dep_saving_Router.js
@@ -64,40 +64,53 @@ depsavingRouter.get("/deposit_list", async (req, res) => {
     }
 });
 
-// save / edit group
-depsavingRouter.post("/save_sbacc", async (req, res) => {
-    try {
-      const {sb_id, tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,created_ip } = req.body;
-      let datetime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-      const table = "bdccb.td_deposit";
-      const columns = sb_id > 0 ? ["tenant_id","shg_id","branch_id","acc_no","acc_opening_dt","balance","modified_by","modified_at","modified_ip"] : ["tenant_id","shg_id","branch_id","acc_no","acc_opening_dt","balance","created_by","created_at","created_ip"];
-      const values = sb_id > 0 ? [tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,datetime,created_ip] : [tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,datetime,created_ip];
-      const whereColumns = sb_id > 0 ? ["sb_id"] : [];
-      const whereValues = sb_id > 0 ? [sb_id] : [];
-      const flag = sb_id > 0 ? 1 : 0;
-      const result = await saveRecord(table, columns, values,whereColumns,whereValues,flag);
-      return res.send({
-        success: true,
-        msg: sb_id > 0 ? "Record Updated Successfully" : "Record Inserted Successfully",
-        data: result.lastId
-      });
-      } catch (error) {
-        console.error("Error in while save group:", error);
-        return res.send({
-        success: false,
-        msg: "Internal server error",
-        errorCode: "SERVER_ERROR"
-       });
-      }
-});
+    // save / edit group
+    depsavingRouter.post("/save_sbacc", async (req, res) => {
+        try {
+          const {sb_id, tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,created_ip } = req.body;
+          let datetime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+          const table = "bdccb.td_deposit";
+          const columns = sb_id > 0 ? ["tenant_id","shg_id","branch_id","acc_no","acc_opening_dt","balance","modified_by","modified_at","modified_ip"] : ["tenant_id","shg_id","branch_id","acc_no","acc_opening_dt","balance","created_by","created_at","created_ip"];
+          const values = sb_id > 0 ? [tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,datetime,created_ip] : [tenant_id,shg_id,branch_id,acc_no,acc_opening_dt,balance,created_by,datetime,created_ip];
+          const whereColumns = sb_id > 0 ? ["sb_id"] : [];
+          const whereValues = sb_id > 0 ? [sb_id] : [];
+          const flag = sb_id > 0 ? 1 : 0;
+          const result = await saveRecord(table, columns, values,whereColumns,whereValues,flag);
+          return res.send({
+            success: true,
+            msg: sb_id > 0 ? "Record Updated Successfully" : "Record Inserted Successfully",
+            data: result.lastId
+          });
+          } catch (error) {
+            console.error("Error in while save group:", error);
+            return res.send({
+            success: false,
+            msg: "Internal server error",
+            errorCode: "SERVER_ERROR"
+          });
+          }
+    });
     get_acount_balance = async (sb_id) => {
-        const select = `balance`;
-        const table_name = "bdccb.td_deposit_trans";
-        const whr = `sb_id = '${sb_id}' order by trans_no desc limit 1 `;
-        const order = null; 
-        const res_dt = await db_Select(select, table_name, whr, order);
-        const balance = res_dt.msg[0].balance;
-        return balance; // INTEGER
+        //  First check first transaction OR NOT  // 
+          const first_trans_result = await db_Select('count(*) as count ', 'bdccb.td_deposit_trans', `sb_id = '${sb_id}'`, null);
+          if(first_trans_result.suc === 1 && first_trans_result.msg[0].count === '0') {
+
+            const inital_balance = await db_Select('balance', 'bdccb.td_deposit', `sb_id = '${sb_id}'`, null);
+            if(inital_balance.suc === 1 && inital_balance.msg.length > 0) {
+              return inital_balance.msg[0].balance; // INTEGER
+            }else{
+              return 0;
+            }
+          // No transactions found, return 0 balance
+        }else{
+              const select = `balance`;
+              const table_name = "bdccb.td_deposit_trans";
+              const whr = `sb_id = '${sb_id}' order by trans_no desc limit 1 `;
+              const order = null; 
+              const res_dt = await db_Select(select, table_name, whr, order);
+              const balance = res_dt.msg[0].balance;
+              return balance; // INTEGER
+        }
     }
     depsavingRouter.post("/save_dept_trans", async (req, res) => {
         try {
