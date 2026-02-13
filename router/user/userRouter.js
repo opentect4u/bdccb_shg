89a0409eefa6b41537d7df0204d27941c88d4439
meettan <mondal.tanmoy@synergicsoftek.com>
2026-02-13 16:06:26 +0530

    chnage

--- router/user/userRouter.js
+++ router/user/userRouter.js
@@ -101,46 +101,14 @@ userRouter = express.Router();
         }
                     
     });
-    userRouter.post("/save_user_bkp", async (req, res) => {
-         const {add_edit_flag,user_id,branch_id, user_type,active_flag, user_name,pwd, phone_mobile,ip_address,created_by,designation } = req.body;
-        try {
-            const user_type_list  = ['B','P','S','H'];
-            const tenant_id = await get_tenant_id(branch_id);   
-            
-            if(!user_type || !user_type_list.includes(user_type)){
-                validationError(res, "user type is required and must be one of B, P, S, H,PU,BU");
-            }else if(!user_name || user_name.trim() === ""){
-                validationError(res, "user name is required");
-            }
-            const hashedPassword = await bcrypt.hash(pwd, 10);
-            const table = "bdccb.md_user";
-            const columns = add_edit_flag > 0 ? ["tenant_id","brn_code","user_type","user_name","phone_mobile","ip_address","designation"] : ["user_id","tenant_id","brn_code","user_type","user_name","phone_mobile","active_flag","password","created_by","created_at","ip_address","designation"];
-            const values = add_edit_flag > 0 ? [tenant_id, branch_id, user_type, user_name, phone_mobile, ip_address, designation] : [user_id.trim(), tenant_id, branch_id, user_type, user_name, phone_mobile, 'N', hashedPassword, created_by, new Date(), ip_address,designation ];
-            const whereColumns = add_edit_flag > 0 ? ["user_id"] : [];
-            const whereValues = add_edit_flag > 0 ? [user_id] : [];
-            const flag = add_edit_flag > 0 ? 1 : 0; // 0 for insert, 1 for update
-            const result = await saveRecord(table, columns, values,whereColumns,whereValues,flag);
-            return res.send({
-                success: true,
-                msg: add_edit_flag > 0 ? "Record Updated Successfully" : "Record Inserted Successfully",
-                data: result.lastId
-            });
-        } catch (error) {
-            console.error("Error in /save_user route:", error);
-            return res.send({
-            success: false,
-            msg: "Internal server error",
-            errorCode: "SERVER_ERROR"
-            });
-        }
-    });
+   
     userRouter.post("/changepass", async (req, res) => {
-        const {old_pass,new_pass,user_id } = req.body;
+        const {old_pass,new_pass,user_id,created_by,ip_address } = req.body;
         try {
                 if(old_pass != new_pass){
                     var whr = `user_id='${user_id}' AND active_flag='Y'`;
                     var res_dt = await db_Select("*", "bdccb.md_user", whr, null);
-
+                  let datetime = new Date().toISOString().slice(0, 19).replace('T', ' ');
                     if (res_dt.msg.length > 0) {
                             //const hasLowercase = /[a-z]/.test(new_pass);
                             const hasUppercase = /[A-Z]/.test(new_pass);
@@ -152,8 +120,8 @@ userRouter = express.Router();
                                 var pass = await bcrypt.hash(new_pass, 10);
                                 var ip = req.clientIp;
                                 const table = "bdccb.md_user";
-                                const columns = ["password"];
-                                const values = [pass] 
+                                const columns = ["password","modified_by","modified_at","ip_address"];
+                                const values = [pass,created_by,datetime,ip_address] 
                                 const whereColumns = ["user_id"] ;
                                 const whereValues = [user_id];
                         
@@ -235,17 +203,17 @@ userRouter = express.Router();
             });
         }
     });
-    userRouter.post("/verifyuser", async (req, res) => {
-        const {user_id,active_flag,default_pass,approve_by,approve_ip} = req.body;
+    userRouter.post("/profile_update", async (req, res) => {
+        const {user_id,designation,created_by,ip_address} = req.body;
         try {
                 var whr = `user_id='${user_id}'`;
                 var res_dt = await db_Select("*", "bdccb.md_user", whr, null);
-                const hashedPassword = await bcrypt.hash('bdccb@1234', 10);
+               
                 if (res_dt.msg.length > 0) {
                     
                     const table = "bdccb.md_user";
-                    const columns = default_pass > 0 ? ["active_flag","password","approved_by","approved_dt","approved_ip"] : ["active_flag","approved_by","approved_dt","approved_ip"];
-                    const values = default_pass > 0 ? [active_flag,hashedPassword,approve_by,new Date().toISOString().slice(0, 19).replace('T', ' '),approve_ip] : [active_flag,approve_by,new Date().toISOString().slice(0, 19).replace('T', ' '),approve_ip];
+                    const columns = user_id.length > 0 ? ["designation","modified_by","modified_at","ip_address"] : ["designation","modified_by","modified_at","ip_address"];
+                    const values = user_id.length > 0 ? [designation,created_by,new Date().toISOString().slice(0, 19).replace('T', ' '),ip_address] : [designation,created_by,new Date().toISOString().slice(0, 19).replace('T', ' '),ip_address];
                     const whereColumns = ["user_id"];
                     const whereValues = [user_id];
                     const result = await saveRecord(table, columns, values,whereColumns,whereValues,1);
--- server.js
+++ server.js
@@ -24,92 +24,6 @@ app.use('/v1/dashboard', require('./router/dashboard/indexDashboardRouter'));
 app.use('/v1/user', require('./router/user/indexUserRouter'));
 
 
-// app.post("/v1/login", async (req, res) => {
-//     try {
-//         const user = { username: "admin", password: "admin123" };
-//         const { username, password } = req.body;
-
-//          if (!username || !password) {
-//       return res.json({
-//         success: false,
-//         msg: "Username and password are required"
-//         });
-//         }
-
-//         if (username === user.username && password === user.password) {
-//              const userData = { username:username, age: "30", role: "Backend Developer" };
-//             const jwtToken = await createToken(userData);
-//             // const user_dtls = {
-//             //               "emp_id": 9999,
-//             //               "brn_code": "100",
-//             //               "id": 4,
-//             //               "user_type": "Super Admin",
-//             //               "session_id": 8585858585,
-//             //               "emp_name": "Test EMP",
-//             //               "phone_home": 0,
-//             //               "phone_mobile": 0,
-//             //               "email": "",
-//             //               "gender": "M",
-//             //               "active_flag": "Y",
-//             //               "area_code": 0,
-//             //               "branch_name": "SSVWS",
-//             //               "dist_code": 10021,
-//             //               "transaction_date": "2026-01-15"
-//             //            };
-//             var select = "a.tenant_id,b.dist_id,a.tenant_name,c.dist_name",
-//             table_name = "md_tenant a LEFT JOIN md_tenant_dist b ON a.tenant_id = b.tenant_id LEFT JOIN md_district c ON b.dist_id = c.dist_code",
-//             whr = null,
-//             order = null;
-//             var res_dt = await db_Select(select,table_name,whr,order);
-
-//              const district_list = res_dt.msg.map(row => ({
-//               dist_code: row.dist_id,
-//               dist_name: row.dist_name
-//               }));
-
-//              const user_dtls = {
-//                 district_list: district_list,
-//                 tenant_id: res_dt.msg[0].tenant_id,
-//                 tenant_name: res_dt.msg[0].tenant_name,
-//                  emp_id: 9999,
-//                 brn_code: "2",
-//                 id: 4,
-//                 user_type: "Super Admin",
-//                 session_id: 8585858585,
-//                 emp_name: "Test EMP",
-//                 phone_home: 0,
-//                 phone_mobile: 0,
-//                 email: "",
-//                 gender: "M",
-//                 active_flag: "Y",
-//                 area_code: 0,
-//                 branch_name: "SSVWS",
-//                 transaction_date: "2026-01-15"
-//               };
-//             return res.send({
-//                 success: true,
-//                 msg: "Login Successful",
-//                 user_dtls: [user_dtls],
-//                 token: jwtToken.token,
-//                 refresh_token: jwtToken.token
-//             });
-//         }else{
-//              return res.send({
-//                 success: false,
-//                 msg: "Invalid Credentials"
-//                 });
-//         }
-
-//     } catch (error) {
-//         console.error("Error in /login route:", error);
-//         return res.send({
-//         success: false,
-//         msg: "Internal server error",
-//         errorCode: "SERVER_ERROR"
-//         });
-//     }
-// });
-
 app.post("/v1/login", async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -122,7 +36,7 @@ app.post("/v1/login", async (req, res) => {
     }
 
     // FETCH USER
-    let select = `a.user_id emp_id,a.user_name emp_name,a.brn_code,a.user_type,a.phone_mobile,a.active_flag,a.password,a.session_id,b.branch_name,c.tenant_id,c.tenant_name,d.dist_id,e.dist_name`;
+    let select = `a.user_id emp_id,a.user_name emp_name,a.designation,a.brn_code,a.user_type,a.phone_mobile,a.active_flag,a.password,a.session_id,b.branch_name,c.tenant_id,c.tenant_name,d.dist_id,e.dist_name`;
 
     let table_name = `bdccb.md_user a LEFT JOIN public.md_branch b ON a.brn_code = b.branch_id
       LEFT JOIN public.md_tenant c ON a.tenant_id = c.tenant_id
@@ -165,6 +79,7 @@ app.post("/v1/login", async (req, res) => {
       tenant_name: userData.tenant_name,
       emp_id: userData.emp_id,
       emp_name: userData.emp_name,
+      designation: userData.designation,
       brn_code: userData.brn_code,
       branch_name: userData.branch_name,
       user_type: userData.user_type,
