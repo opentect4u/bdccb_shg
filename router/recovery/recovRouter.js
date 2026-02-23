a9b59637c370617cacedd020133eca84a1b02eea
meettan <mondal.tanmoy@synergicsoftek.com>
2026-02-23 14:56:47 +0530

    chnage

--- router/recovery/recovRouter.js
+++ router/recovery/recovRouter.js
@@ -83,10 +83,11 @@ recovRouter.post("/fetch_pacs_shg_details", async (req, res) => {
 // SAVE DISBURSEMENT (BRANCH -> PACS)
 recovRouter.post("/save_recovery", async (req, res) => {
   try {
-    const { trans_id,tenant_id,branch_id,loan_acc_no,loan_to,branch_shg_id,recov_dt,recov_amt,created_by,ip_address,loan_id,tran_id,curr_prn_recov,curr_intt_recov,curr_prn,curr_intt } = req.body;
+    const { trans_id,tenant_id,branch_id,loan_acc_no,loan_to,branch_shg_id,recov_dt,recov_amt,created_by,ip_address,loan_id,tran_id,curr_prn_recov,curr_intt_recov,curr_prn,curr_intt,members } = req.body;
 
     let datetime = new Date().toISOString().slice(0, 19).replace("T", " ");
     let transid = trans_id > 0 ? trans_id : await transaction_id();
+    // ***** INSERT/UPDATE GROUP LOAN TRANSACTION TABLE  *****
     var table = "bdccb.td_loan_transactions";
     var columns =
       trans_id > 0 ? ["trans_dt","loan_to","branch_shg_id","loan_ac_no","cr_amt","curr_prn","modified_by","modified_dt","ip_address"] : ["trans_dt","trans_id","tenant_id","loan_to","branch_shg_id","loan_id","loan_ac_no","trans_type","dr_amt","cr_amt","curr_prn_recov","curr_intt_recov","ovd_prn_recov","ovd_intt_recov","curr_prn","curr_intt","ovd_prn","ovd_intt","approval_status","created_by","created_dt","ip_address"];
@@ -94,35 +95,60 @@ recovRouter.post("/save_recovery", async (req, res) => {
       trans_id > 0
         ? [recov_dt,loan_to,branch_shg_id,loan_acc_no,recov_amt,curr_prn_recov,created_by,datetime,ip_address,]: [recov_dt,transid,tenant_id,loan_to,branch_shg_id,loan_id,
           loan_acc_no,"R",0,recov_amt,curr_prn_recov,curr_intt_recov,0,0,curr_prn,curr_intt,0,0,"U",created_by,datetime,ip_address];
-    var whereColumns = trans_id > 0 ? ["trans_id", "tenant_id", "loan_id"] : [];
-    var whereValues = trans_id > 0 ? [tran_id, tenant_id, loan_id] : [];
-    var flag = trans_id > 0 ? 1 : 0;
-    var trans_result = await saveRecord(
-      table,
-      columns,
-      values,
-      whereColumns,
-      whereValues,
-      flag,
-    );
+        var whereColumns = trans_id > 0 ? ["trans_id", "tenant_id", "loan_id"] : [];
+        var whereValues = trans_id > 0 ? [tran_id, tenant_id, loan_id] : [];
+        var flag = trans_id > 0 ? 1 : 0;
+        var trans_result = await saveRecord(table,columns,values,whereColumns,whereValues,flag);
+    // ***** INSERT/UPDATE GROUP LOAN TRANSACTION TABLE  *****
 
-    if (!trans_result || trans_result.suc !== 1) {
+       //  ********  UPDATE GROUP LOAN TABLE  ******** 
+
+        const group_loan_columns = ["curr_prn","curr_intt","modified_by","modified_dt","ip_address"];
+        const group_loan_values = [curr_prn,curr_intt,created_by,datetime,ip_address];
+        const group_loan_whereColumns = ["loan_id"];
+        const group_loan_whereValues = [loan_id]; 
+         var group_loan = await saveRecord("bdccb.td_loan",group_loan_columns,group_loan_values, group_loan_whereColumns,group_loan_whereValues,1);
+
+         //  ********  UPDATE GROUP LOAN TABLE  ********
+
+      for(meb of members) {
+       let member_transid = meb.memb_trans_id > 0 ? meb.memb_trans_id : await transaction_id();
+        var member_current_prn = meb.memb_curr_prn - meb.memb_recov_prin;
+        var member_current_intt = meb.memb_curr_intt - meb.memb_recov_intt;
+
+        ///  ********  INSERT/UPDATE MEMBER TRANSACTION TABLE  ********
+        const member_columns = meb.memb_trans_id > 0 ? ["trans_dt","dr_amt","curr_prn","modified_by","modified_dt","ip_address"] : ["loan_id","trans_dt","trans_id","tenant_id","branch_id","trans_type","dr_amt","cr_amt","curr_prn_recov","curr_intt_recov","ovd_prn_recov","ovd_intt_recov","curr_prn","curr_intt","ovd_prn","ovd_intt","approval_status","created_by","created_dt","ip_address"];
+        const member_values = meb.memb_trans_id > 0 ? [recov_dt,meb.memb_recov_amt,memb_current_prn,created_by,datetime,ip_address] : [meb.memb_loan_id,recov_dt,member_transid,tenant_id,branch_shg_id,"R",0,meb.memb_recov_prin,meb.memb_recov_prin,meb.memb_recov_intt,0,0,member_current_prn,member_current_intt,meb.memb_ovd_prn,meb.memb_ovd_intt,"U",created_by,datetime,ip_address];
+        const member_whereColumns = meb.memb_trans_id > 0 ? ["memb_trans_id"] : [];
+        const member_whereValues = meb.memb_trans_id > 0 ? [meb.memb_trans_id] : []; 
+        const member_flag = meb.memb_trans_id > 0 ? 1 : 0; 
+        var member_result = await saveRecord("bdccb.td_loan_member_trans",member_columns,member_values, member_whereColumns,member_whereValues,member_flag);
+        ///  ********  INSERT/UPDATE MEMBER TRANSACTION TABLE  ********
+
+        //  ********  UPDATE MEMBER LOAN TABLE  ******** 
+        const member_loan_columns = ["prn_amt","modified_by","modified_at","ip_address"];
+        const member_loan_values = [member_current_prn,created_by,datetime,ip_address];
+        const member_loan_whereColumns = ["loan_id","member_code"];
+        const member_loan_whereValues = [meb.memb_loan_id,meb.member_code]; 
+         var member_loan = await saveRecord("bdccb.td_loan_member",member_loan_columns,member_loan_values, member_loan_whereColumns,member_loan_whereValues,1);
+         //  ********  UPDATE MEMBER LOAN TABLE  ********
+
+        console.log("Member Transaction Result:", member_result);
+      }
+        // if (!trans_result || trans_result.suc !== 1 || !member_result || member_result.suc !== 1) {
+        //   return res.send({
+        //     success: true,
+        //     msg:trans_result.msg || loan_id > 0 ? "Failed to edit loan in transaction table": "Failed to save loan in transaction table",
+        //     data: [],
+        //   });
+        // }
       return res.send({
         success: true,
         msg:
-          trans_result.msg || loan_id > 0
-            ? "Failed to edit loan in transaction table"
-            : "Failed to save loan in transaction table",
-        data: [],
+          trans_id > 0
+            ? "Recovery edit Done Successfully"
+            : "Recovery Done Successfully",
       });
-    }
-    return res.send({
-      success: true,
-      msg:
-        trans_id > 0
-          ? "Recovery edit Done Successfully"
-          : "Recovery Done Successfully",
-    });
   } catch (error) {
     console.error("Error in while save recovery:", error);
     return res.send({
