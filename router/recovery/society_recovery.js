82eaa18ca4cb208b6b90884392b3d08562bc7718
Sayantika Dhar <sayantika.dhar04@gmail.com>
2026-03-14 12:10:36 +0530

    commit write an api to fetch society recovery details 14.03.2026

--- router/recovery/society_recovery.js
+++ router/recovery/society_recovery.js
@@ -324,11 +324,15 @@ if (fetch_society_recovery_data.suc === 1 && fetch_society_recovery_data.msg.len
 
 var select_mem_trans = "TO_CHAR(a.trans_date, 'YYYY-MM-DD') AS trans_date,a.trans_id,a.loan_id,(COALESCE(a.dr_amt,0)) AS debit_amount,(COALESCE(a.cr_amt,0)) AS credit_amount,(COALESCE(a.curr_prn_recov,0)) AS curr_prn_recov,(COALESCE(a.curr_intt_recov,0)) AS curr_intt_recov",
 table_mem_trans = "bdccb.td_loan_member_trans a",
-whr_mem_trans = `a.ccb_loan_id = '${ccb_loan_id}' AND a.tenant_id = '${tenant_id}' AND a.trans_type IN ('I','R')`,
+whr_mem_trans = `a.ccb_loan_id = '${ccb_loan_id}' AND a.tenant_id = '${tenant_id}' 
+AND a.trans_type IN ('I','R') AND a.trans_dt::date BETWEEN '${from_dt}' AND '${to_dt}'`,
 order_mem_trans = null;
 var fetch_society_mem_recov_dtls = await db_Select(select_mem_trans,table_mem_trans,whr_mem_trans,order_mem_trans);
 
-fetch_society_recovery_data.msg[0].members = fetch_society_mem_recov_dtls.msg || [];
+fetch_society_recovery_data.msg[0].members =
+  fetch_society_mem_recov_dtls.suc === 1
+    ? fetch_society_mem_recov_dtls.msg
+    : [];
 
 return res.send({
     success: true,
