34b338469c24eb15e76687a5bfc2d676ef32e953
meettan <mondal.tanmoy@synergicsoftek.com>
2026-03-31 14:39:06 +0530

    chnage

--- router/dashboard/dashboardRouter.js
+++ router/dashboard/dashboardRouter.js
@@ -152,10 +152,11 @@ try{
 dashboardRouter.post("/dashboard_grp_loan_bal", async (req, res) => {
   try{
     const {emp_id,tenant_id} = req.body;
-
+    let parts = emp_id.split("-");
+    let group_codes = parts[2];
     var select = "a.group_code,a.group_name,b.loan_to",
     table_name = "bdccb.md_group a LEFT JOIN bdccb.td_loan_member b ON a.group_code = b.group_code",
-    whr = `a.phone1 = '${emp_id}'`,
+    whr = `b.group_code = '${group_codes}'`,
     order = null;
     var fetch_grp_code = await db_Select(select,table_name,whr,order);
 
