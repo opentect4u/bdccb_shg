39906f865d54abd1f8e95c3509fb0c1c86f051fb
Sayantika Dhar <sayantika.dhar04@gmail.com>
2026-04-14 16:59:39 +0530

    commit ccb level re-finance data 13.04.2026

--- router/refinance/refinanceRouter.js
+++ router/refinance/refinanceRouter.js
@@ -537,10 +537,11 @@ refinanceRouter.post("/fetch_unapprove_re-finance_data_branch_level", async (req
 refinanceRouter.post("/approve_re-finance_branch", async (req, res) => {
   try{
   const {loan_id,tenant_id,branch_id,trans_id,group_code,curr_roi,penal_roi,period,disb_dt,created_by} = req.body;
+  console.log(req.body,'approve_branch');
   
   let datetime = new Date().toISOString().slice(0, 19).replace("T", " ");
   var pay_mode = "Monthly";
-  
+
   let instl_date = await genDate(disb_dt, period, pay_mode);
   const startDate = instl_date.emtStart;
   const endDate = instl_date.emiEnd;
